c776138f892b51e398c41ad5bfc8b8e378b34eb1
katua <katuacyril@gmail.com>
2025-07-23 16:03:05 +0300

    add useEffect code

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,12 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    if (timeRemaining === 0) {
+      onAnswered(false);
+      return; // Stop setting more timers
+    }
+
+    const timerId = setTimeout(() => {
+      setTimeRemaining(timeRemaining - 1);
+    }, 1000);
+
+    return () => clearTimeout(timerId);
+  }, [timeRemaining, onAnswered]);
 
   function handleAnswer(isCorrect) {
-    setTimeRemaining(10);
+    setTimeRemaining(10); 
     onAnswered(isCorrect);
   }
 
